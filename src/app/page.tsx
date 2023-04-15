8284b29bd50ce9865d15b7f23700974c56b95994
henriquemattia <mattia.henrique@gmail.com>
2023-04-15 10:14:18 -0300

    Initializing Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,50 +47,81 @@ export default function Home() {
           </div>
 
           <h1 className="font-semibold text-3xl mt-10">Good afternoon</h1>
-
+          {/* 6 Playlists no topo */}
           <div className="grid grid-cols-3 gap-4 mt-4">
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album"/>
+              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album" />
               <strong>Wating Light</strong>
               <button className="h-12 w-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
-           </a>
+            </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album"/>
+              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album" />
               <strong>Wating Light</strong>
               <button className="h-12 w-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
-           </a>
+            </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album"/>
+              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album" />
               <strong>Wating Light</strong>
               <button className="h-12 w-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
-           </a>
+            </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album"/>
+              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album" />
               <strong>Wating Light</strong>
               <button className="h-12 w-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
-           </a>
+            </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album"/>
+              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album" />
               <strong>Wating Light</strong>
               <button className="h-12 w-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
-           </a>
+            </a>
             <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album"/>
+              <Image src="/album.jpeg" width={104} height={104} alt="Capa de albumCapa de album" />
               <strong>Wating Light</strong>
               <button className="h-12 w-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
-           </a>
+            </a>
+
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">Made for Henrique Fernandes</h2>
+
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpeg" className="w-full" width={120} height={120} alt="Capa de albumCapa de album" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallons, COIN, girl in red and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpeg" className="w-full" width={120} height={120} alt="Capa de albumCapa de album" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallons, COIN, girl in red and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpeg" className="w-full" width={120} height={120} alt="Capa de albumCapa de album" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallons, COIN, girl in red and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpeg" className="w-full" width={120} height={120} alt="Capa de albumCapa de album" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallons, COIN, girl in red and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10">
+              <Image src="/album.jpeg" className="w-full" width={120} height={120} alt="Capa de albumCapa de album" />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallons, COIN, girl in red and more</span>
+            </a>
 
           </div>
 
